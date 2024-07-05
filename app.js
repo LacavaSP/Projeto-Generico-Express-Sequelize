96494d792876ca6f92d8e05194ba784d3fc607a1
LacavaSP <deitylink38@gmail.com>
2024-07-04 22:35:28 -0300

    verificação de integridade de arquivo de configuração do servidor

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require('path')
 const express = require('express')
 
 const servidorHTTPBackend = express()
+let arquivoDeConfiguracaoDoServidor = null
 
-const arquivoDeConfiguracaoDoServidor = JSON.parse(fs.readFileSync(path.join(__dirname, '/config.json'), 'utf-8')) 
+try {
+    arquivoDeConfiguracaoDoServidor = JSON.parse(fs.readFileSync(path.join(__dirname, '/config.json'), 'utf-8')) 
+} catch(e) {
+    console.log(e)
+    throw new Error('Erro ao processar o arquivo de configurações do servidor')
+}
+ 
 const portaProjeto = arquivoDeConfiguracaoDoServidor.porta
 const host = arquivoDeConfiguracaoDoServidor.host
 
 servidorHTTPBackend.listen(portaProjeto, host,() => {
-    console.log('Servidor HTTP sendo escutado')
+    console.log(`Servidor HTTP sendo escutado em http://${host}:${portaProjeto}/` )
 })
