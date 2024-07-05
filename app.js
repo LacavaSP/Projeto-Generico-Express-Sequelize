9298a2de353c85a6aafb668863cf2a1a710b00a5
LacavaSP <deitylink38@gmail.com>
2024-07-04 22:31:45 -0300

    modularizacao das configuracoes da aplicação em um arquivo JSON

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const fs = require('fs')
+const path = require('path')
 const express = require('express')
-const portaProjeto = 6061
+
 const servidorHTTPBackend = express()
 
-servidorHTTPBackend.listen(portaProjeto, () => {
+const arquivoDeConfiguracaoDoServidor = JSON.parse(fs.readFileSync(path.join(__dirname, '/config.json'), 'utf-8')) 
+const portaProjeto = arquivoDeConfiguracaoDoServidor.porta
+const host = arquivoDeConfiguracaoDoServidor.host
+
+servidorHTTPBackend.listen(portaProjeto, host,() => {
     console.log('Servidor HTTP sendo escutado')
 })
